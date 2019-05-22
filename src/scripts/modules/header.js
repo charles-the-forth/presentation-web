523205ac2a20df3d48e2a18481723d4bc82d731b
Václav Pavlíček <venda.pavlicek@gmail.com>
2019-05-22 17:39:10 +0200

    Update background image resizing.

--- src/scripts/modules/header.js
+++ src/scripts/modules/header.js
@@ -1,11 +1,3 @@
-window.addEventListener('load', () => {
-    const logo = document.getElementById('js-logo');
-    const arrowDownButton = document.getElementById('js-arrow-down-button');
-
-    logo.classList.add('opacity-transition-750ms');
-    setTimeout(() => arrowDownButton.classList.add('opacity-transition-750ms'), 750);
-});
-
 window.scrollDown = function() {
     const newsSection = document.getElementById('js-eva-section');
     window.scrollTo({
@@ -13,3 +5,35 @@ window.scrollDown = function() {
         behavior: 'smooth'
     });
 };
+
+const countBackgroundSize = () => {
+    const backgroundImageHeight = 1440;
+    const backgroundImageWidth = 2560;
+    const headerSection = document.getElementById('js-header-section');
+    if (
+        headerSection.offsetWidth / headerSection.offsetHeight <
+        backgroundImageWidth / backgroundImageHeight
+    ) {
+        headerSection.style.backgroundSize = `auto ${
+            headerSection.offsetHeight
+        }px`;
+    } else {
+        headerSection.style.backgroundSize = `${
+            headerSection.offsetWidth
+        }px auto`;
+    }
+};
+
+window.addEventListener('resize', () => {
+    countBackgroundSize();
+});
+
+window.addEventListener('load', () => {
+    countBackgroundSize();
+    
+    const logo = document.getElementById('js-logo');
+    const arrowDownButton = document.getElementById('js-arrow-down-button');
+
+    logo.classList.add('opacity-transition-750ms');
+    setTimeout(() => arrowDownButton.classList.add('opacity-transition-750ms'), 750);
+});
